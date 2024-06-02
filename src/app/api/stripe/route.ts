328b036aff6dc8f5dea611c6ded44ad36c4c977a
iyz <iy.usa@outlook.com>
2024-06-02 14:59:52 -0400

    add better error handling, debug stripe webhook

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -3,8 +3,8 @@ import prisma from "@/lib/db";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request: Request) {
-  const data = await request.json();
-  const signature = request.headers.get("Stripe-Signature");
+  const data = await request.text();
+  const signature = request.headers.get("stripe-signature");
 
   let event;
   try {
@@ -14,23 +14,28 @@ export async function POST(request: Request) {
       process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (error) {
-    console.log("Webhook verification failed.");
+    console.log("Webhook verification failed", error);
     return Response.json(null, { status: 400 });
   }
 
-  switch (event.type) {
-    case "checkout.session.completed":
-      await prisma.user.update({
-        where: {
-          email: data.data.object.customer_email,
-        },
-        data: {
-          subscriptionActive: true,
-        },
-      });
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}`);
+  try {
+    switch (event.type) {
+      case "checkout.session.completed":
+        await prisma.user.update({
+          where: {
+            email: event.data.object.customer_email,
+          },
+          data: {
+            subscriptionActive: true,
+          },
+        });
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  } catch (error) {
+    console.log("Error updating user subscription status", error);
+    return Response.json(null, { status: 500 });
   }
 
   return Response.json(null, { status: 200 });
